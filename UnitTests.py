45fa10e7f5de0f5665363edb22da8adc1c9a8c03
Carl Martinez <carlmar89@live.com>
2015-05-20 19:38:51 -0700

    added basic framework for unit test
    One test per variable. Each test will have one random variable,
    everything else preset.

---
+++ UnitTests.py
@@ -0,0 +1,46 @@
+import pytest
+
+def test_teacher_classes():
+    pass
+    
+def test_teacher_students():
+    pass
+
+def test_teacher_expertise():
+    pass
+    
+def test_teacher_schedule():
+    pass
+    
+def test_teacher_salary():
+    pass
+    
+def test_teacher_salaryCompound():
+    pass
+    
+def test_course_core():
+    pass
+
+def test_course_timeslot():
+    pass
+    
+def test_course_dayofweek():
+    pass
+    
+def test_course_credits():
+    pass
+    
+def test_course_teacher():
+    pass
+    
+def test_global_price():
+    pass
+    
+def test_global_growth():
+    pass
+    
+def test_global_temps():
+    pass
+    
+def test_global_hiringCost():
+    pass

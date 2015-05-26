7d97218ec03266d219779eb4957b8f07061db3a2
boromw <boromw@uw.edu>
2015-05-25 23:21:37 -0700

    Update Expertise.py

--- Expertise.py
+++ Expertise.py
@@ -6,8 +6,15 @@ class Expertise(object):
     def add(self, course):
         found_abstract = False
         for ab_course in self.abstract_courses:
-                if ab_course.name == course.name and not found_abstract:
+                if ab_course.name in course.name and not found_abstract:
                     ab_course.add(course)
                     found_abstract = True
         if not found_abstract:
             self.abstract_courses.append(AbstractCourse(course))
+    
+    def remove(self, course):
+        target_title = course.title
+        found_abstract = False
+        for ab_course in self.abstract_courses:
+            if target_title in ab_course.name and not found_abstract:
+                ab_course.remove(course)

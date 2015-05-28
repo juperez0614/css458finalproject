e10135aa202cf7a94914fedca6c1696cd5a04300
boromw <boromw@uw.edu>
2015-05-27 19:26:59 -0700

    Update Course.py

--- Course.py
+++ Course.py
@@ -57,11 +57,11 @@ class Course(object):
         """
         return self.time.has_conflict_with(other.time)
     
-"""def openData():
-    data = open(os.getcwd() + '/courses.txt', 'r')
-    individual_lines = data.readlines()
-    cruncher = CourseCruncher()
-    for line in individual_lines:
-        course = Course(line.split("\t")) 
-        cruncher.add(course)
-    return cruncher"""
+    """def openData():
+        data = open(os.getcwd() + '/courses.txt', 'r')
+        individual_lines = data.readlines()
+        cruncher = CourseCruncher()
+        for line in individual_lines:
+            course = Course(line.split("\t")) 
+            cruncher.add(course)
+        return cruncher"""
